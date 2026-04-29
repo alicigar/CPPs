1799753bb01316e6c8b922522b42f235ed0364a9
Alicia Momoko Garcia Gokan <alicigar@c3r8s3.42malaga.com>
2026-04-29 20:50:10 +0200

    missing ex06 of CPP01

---
+++ CPP_00_to_04/CPP_Module_01/ex06/main.cpp
@@ -0,0 +1,36 @@
+/*#include "Harl.hpp"
+
+int getLevel(std::string level)
+{
+    std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    for (int i = 0; i < 4; i++)
+    {
+        if (levels[i] == level)
+            return (i);
+    }
+    return (-1);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		std::cout << "Error: wrong number of arguments, lovely." << std::endl;
+		return (1);
+	}
+
+	Harl harl;
+
+	switch (getLevel(argv[1]))
+    {
+        case 0: harl.debug();
+        case 1: harl.info();
+        case 2: harl.warning();
+        case 3: harl.error();
+                break;
+        default:
+            std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+    }
+
+	return (0);
+}*/
